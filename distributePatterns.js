833378687758ec60209f3f6beb0a514906a45d97
alan-keepaware <alan@keepaware.com>
2023-12-11 14:11:20 -0600

    Trigger test

--- distributePatterns.js
+++ distributePatterns.js
@@ -8,14 +8,16 @@ const excludedFiles = ['default.pattern.json'];
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 // Function to send a POST request
-const sendPostRequest = async (url, payload) => {
+const sendPostRequest = (url, payload) => {
     try {
         const request = `curl -s -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer ${ACCESS_TOKEN}' -d '${payload}' '${url}'`;
-        const response = await execSync(
+        const response = execSync(
             request,
             { encoding: 'utf-8' }
             ).trim();
             console.log('response', response)
+            console.log('s1', JSON.stringify(response, null, 2));
+            console.log('s2' , JSON.stringify(response));
             console.log('success', response.success)
         if (!response.success) {
             console.error('Failed to distribute pattern', response.error);
@@ -26,13 +28,13 @@ const sendPostRequest = async (url, payload) => {
     }
 };
 
-const processFilesFromLatestCommit = async (folderPath) => {
+const processFilesFromLatestCommit = (folderPath) => {
     // Get the list of files changed in the latest commit
     const changedFiles = fs.readFileSync(process.env.CHANGED_FILES_PATH, { encoding: 'utf-8' }).split('\n');
     let executedFiles = 0;
 
     // Process only .json files from the latest commit
-    changedFiles.forEach(async file => {
+    changedFiles.forEach(file => {
         if (file.endsWith('.json')) {
             const filePath = path.join(folderPath, file);
             const fileName = file.split('.')[0];
@@ -40,7 +42,7 @@ const processFilesFromLatestCommit = async (folderPath) => {
             if (!excludedFiles.includes(fileName)) {
                 console.log('Distributing pattern:', filePath)
                 const jsonPayload = fs.readFileSync(filePath, 'utf-8');
-                const response = await sendPostRequest(endpointUrl, jsonPayload);
+                const response = sendPostRequest(endpointUrl, jsonPayload);
                 console.log(`Response for ${fileName}.json: ${response}`);
                 executedFiles++;
             }
